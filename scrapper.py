a740fc0dd494620f0f48dfa310a0c0258a477877
Joao Leal <joaosilvaleal@gmail.com>
2023-03-26 23:16:49 +0100

    Improvements.

--- scrapper.py
+++ scrapper.py
@@ -1,113 +1,77 @@
 import datetime
+from dataclasses import dataclass
+from typing import List
+
 import jinja2
 import requests
-import trio
 from bs4 import BeautifulSoup
+import trio
+import fire
 
-URL = 'https://www.joaoleal.com/'
-BLOG_URL = URL + 'blog'
-
-
-def fetch_blog_posts(url: str) -> requests.Response:
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise ValueError(f"Failed to fetch page with status code {response.status_code}")
-    return response
+URL = "https://www.joaoleal.com/"
+BLOG_URL = URL + "blog"
 
 
-def parse_blog_posts(html: str) -> list:
-    soup = BeautifulSoup(html, 'html.parser')
-    blog_posts = soup.find_all('div', class_='block-blog-list-item')
-    return blog_posts
+@dataclass
+class BlogPost:
+    post_url: str
+    post_title: str
+    post_date: datetime.datetime
 
 
-def extract_post_data(post) -> dict:
-    post_url = URL + post.find('a', class_='block-blog-list-item__content')['href']
-    post_title = post.find('h3', class_='font-primary block-blog-list-item__title').text.strip()
-    post_date = datetime.datetime.strptime(
-        post.find('p', class_='blog-list-item-meta__subtitle').find('span').text.strip(), '%m/%d/%Y')
-    return {
-        "post_url": post_url,
-        "post_date": post_date,
-        "post_title": post_title
-    }
+class BlogScraper:
+    def fetch_blog_posts(self, url: str) -> requests.Response:
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError(f"Failed to fetch page with status code {response.status_code}")
+        return response
+
+    def parse_blog_posts(self, html: str) -> List[BeautifulSoup]:
+        soup = BeautifulSoup(html, "html.parser")
+        return soup.find_all("div", class_="block-blog-list-item")
+
+    def extract_post_data(self, post: BeautifulSoup) -> BlogPost:
+        post_url = URL + post.find("a", class_="block-blog-list-item__content")["href"]
+        post_title = post.find("h3", class_="font-primary block-blog-list-item__title").text.strip()
+        post_date = datetime.datetime.strptime(
+            post.find("p", class_="blog-list-item-meta__subtitle").find("span").text.strip(), "%m/%d/%Y")
+        return BlogPost(post_url, post_title, post_date)
+
+    def get_latest_blog_posts(self) -> List[BlogPost]:
+        response = self.fetch_blog_posts(BLOG_URL)
+        blog_posts = self.parse_blog_posts(response.text)
+        return [self.extract_post_data(post) for post in blog_posts[:2]]
 
 
-def get_latest_blog_posts() -> dict:
-    response = fetch_blog_posts(BLOG_URL)
-    blog_posts = parse_blog_posts(response.text)
+class ReadmeGenerator:
+    @staticmethod
+    def render_template(username: str, posts: List[BlogPost]) -> str:
+        with open("TEMPLATE.md", "r", encoding="utf-8") as file:
+            template = jinja2.Template(file.read())
 
-    post_data = {
-        "post_url_1": "",
-        "post_title_1": "",
-        "post_date_1": datetime.date.today(),
-        "post_url_2": "",
-        "post_title_2": "",
-        "post_date_2": datetime.date.today(),
-    }
+        today = datetime.date.today()
 
-    if len(blog_posts) >= 2:
-        post_1 = extract_post_data(blog_posts[0])
-        post_data["post_url_1"] = post_1["post_url"]
-        post_data["post_title_1"] = post_1["post_title"]
-        post_data["post_date_1"] = post_1["post_date"]
-
-        post_2 = extract_post_data(blog_posts[1])
-        post_data["post_url_2"] = post_2["post_url"]
-        post_data["post_title_2"] = post_2["post_title"]
-        post_data["post_date_2"] = post_2["post_date"]
-
-    return post_data
+        return template.render(
+            post_url_1=posts[0].post_url,
+            post_title_1=posts[0].post_title,
+            post_date_1=posts[0].post_date.date().isoformat(),
+            post_url_2=posts[1].post_url,
+            post_title_2=posts[1].post_title,
+            post_date_2=posts[1].post_date.date().isoformat(),
+            username=username,
+            today=today.isoformat(),
+        )
 
 
-def generate_readme(
-        username: str,
-        post_url_1: str,
-        post_title_1: str,
-        post_date_1: datetime.datetime,
-        post_url_2: str,
-        post_title_2: str,
-        post_date_2: datetime.datetime,
-):
-    with open("TEMPLATE.md", "r", encoding="utf-8") as file:
-        template = jinja2.Template(file.read())
-
-    today = datetime.date.today()
-
-    return template.render(
-        post_url_1=post_url_1,
-        post_title_1=post_title_1,
-        post_date_1=post_date_1.date().isoformat(),
-        post_url_2=post_url_2,
-        post_title_2=post_title_2,
-        post_date_2=post_date_2.date().isoformat(),
-        username=username,
-        today=today.isoformat(),
-    )
-
-
-async def main(
-        username: str,
-):
-    latest_blog_posts = get_latest_blog_posts()
-
-    print(generate_readme(username,
-                          latest_blog_posts['post_url_1'],
-                          latest_blog_posts['post_title_1'],
-                          latest_blog_posts['post_date_1'],
-                          latest_blog_posts['post_url_2'],
-                          latest_blog_posts['post_title_2'],
-                          latest_blog_posts['post_date_2'],
-                          ))
+async def main(username: str):
+    scraper = BlogScraper()
+    latest_blog_posts = scraper.get_latest_blog_posts()
+    readme = ReadmeGenerator.render_template(username, latest_blog_posts)
+    print(readme)
 
 
 if __name__ == "__main__":
-    import fire
-
-
-    def cli(
-            username: str,
-    ):
+    def cli(username: str):
         """Provide a CLI for the script for use by Fire."""
         trio.run(main, username)
 
